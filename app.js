e54029db23ecb20c101d18df9c46e9c7ca5462d6
John Doe <hallersorel@gmail.com>
2025-01-09 17:09:56 +0200

    + appendChild (product, h3 , price)

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const BASE_URL = "https://fakestoreapi.com/";
-
-const fetchAllProducts = async () => () {
-    const response = await fetch("{BASE_URL}/products");
+const BASE_URL = `https://fakestoreapi.com`;
+let products = [];
+let categories = [];
+const fetchAllProducts = async () => {
+    const response = await fetch(`${BASE_URL}/products`)
     const data = await response.json();
     return data;
 };
-
 const fetchAllCategories = async () => {
-    const response = await fetch("${BASE_URL}/categories");
+    const response = await fetch(`${BASE_URL}/products/categories`)
     const data = await response.json();
-    return data
+    return data;
+};
+const fetchProductsByCategory = async (category) => {
+    const response = await fetch(`${BASE_URL}/products/category/${category}`);
+    const data = await response.json();
+    return data;
+};
+products = await fetchAllProducts();
+categories = await fetchAllCategories();
+
+const showAllProducts = () => {
+    products.forEach((product) => {
+        renderProductCard(product);
+    })
 };
 
-const fetchAllProductsByCategory = async (category) => {
-    const response = await fetch("${BASE_URL}/categories/${category}");
-    const data = await response.json();
-    return data
+const renderProductCard = (product) => {
+    const articleElement = document.createElement("article");
+
+
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+
+
+    const h3Element = document.createElement("h3");
+    h3Element.innerText = product.title;
+
+    const priceElement = document.createElement("span");
+    priceElement.innerText = product.price;
+
+    articleElement.appendChild(productImage);
+    articleElement.appendChild(h3Element);
+    articleElement.appendChild(priceElement);
+
 };
-
-
-
-await fetchAllProducts();
