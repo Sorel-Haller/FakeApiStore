d2d1efa78e00354d7b4c5df650a4190b2d1ffa7d
Sorel-Haller <hallersorel@gmail.com>
2025-01-29 14:35:09 +0200

    algne töötab

--- app.js
+++ app.js
@@ -1,50 +1,53 @@
 const productContainer = document.querySelector('.products');
 
-
-const BASE_URL = `https://fakestoreapi.com`;
+const BASE_URL = 'https://fakestoreapi.com';
 let products = [];
 let categories = [];
-const fetchAllProducts = async () => {
-    const response = await fetch(`${BASE_URL}/products`)
+const fetchProduct = async ()=> {
+    const response = await fetch(`${BASE_URL}/products`);
     const data = await response.json();
     return data;
 };
-const fetchAllCategories = async () => {
-    const response = await fetch(`${BASE_URL}/products/categories`)
-    const data = await response.json();
-    return data;
-};
-const fetchProductsByCategory = async (category) => {
-    const response = await fetch(`${BASE_URL}/products/category/${category}`);
-    const data = await response.json();
-    return data;
-};
-products = await fetchAllProducts();
-categories = await fetchAllCategories();
 
+const fetchAllGategories = async () => {
+    const response = await fetch(`${BASE_URL}/products/categories`);
+    const data = await response.json();
+    return data;
+};
+const fetchProductByCategory = async (category) =>{
+    const response = await fetch(`${BASE_URL}/products/category/${category}`);
+    const data = await response.json()
+    return data;
+};
+
+products = await fetchProduct();
+categories = await fetchAllGategories();
 
 const renderProductCard = (product) => {
-    const articleElement = document.createElement("article");
-
-    const productImage = document.createElement("img");
+    const articleElement = document.createElement('article');
+    
+    
+    const productImage = document.createElement('img');
     productImage.src = product.image;
-
-    const h3Element = document.createElement("h3");
+    
+    const h3Element = document.createElement('h3');
     h3Element.innerText = product.title;
-
-    const priceElement = document.createElement("span");
+    
+    const priceElement = document.createElement('span');
     priceElement.innerText = product.price;
-
+    
     articleElement.appendChild(productImage);
     articleElement.appendChild(h3Element);
     articleElement.appendChild(priceElement);
+    
+    return articleElement;
 };
 
 const showAllProducts = () => {
     products.forEach((product) => {
-        renderProductCard(product);
+        const productCard = renderProductCard(product);
         productContainer.appendChild(productCard);
-    })
+    });
 };
 
 showAllProducts();
